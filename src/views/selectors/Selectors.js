fab71246cd4f816af3f9ba159cf742849b958d2c
Uplift <git@upliftingcode.com>
2014-10-29 13:49:30 +0000

    Add Selector Mixin

---
+++ src/views/selectors/Selectors.js
@@ -0,0 +1,149 @@
+( function( root, factory ) {
+    // Set up appropriately for the environment.
+    // Start with AMD.
+    if ( typeof define === 'function' && define.amd ) {
+        define(
+            [
+                'underscore'
+            ],
+            function( _ ) {
+                root.ExoSuit = root.ExoSuit || {};
+                root.ExoSuit.Mixins = root.ExoSuit.Mixins || {};
+                return ( root.ExoSuit.Mixins.SelectorMixin = factory( root, {}, _ ) );
+            }
+        );
+    // Next for Node.js or CommonJS.
+    } else if ( typeof exports !== 'undefined' ) {
+        var _ = require( 'underscore' );
+
+        factory( root, exports, _ );
+    // Finally, as a browser global.
+    } else {
+        root.ExoSuit = root.ExoSuit || {};
+        root.ExoSuit.Mixins = root.ExoSuit.Mixins || {};
+        root.ExoSuit.Mixins.SelectorMixin = factory( root, {}, root._ );
+    }
+}( this, function( root, SelectorMixin, _ ) {
+    SelectorMixin = function() {
+        // Cache current mixin methods to use later
+        var oldInitialize = this.initialize,
+            oldDelegateEvents = this.delegateEvents,
+            oldRemove = this.remove;
+
+        // Override constructor
+        this.initialize = function() {
+            // Create empty cached selectors object
+            this._selectors = {};
+            // Call Backbone View initialization
+            if ( oldInitialize ) {
+                oldInitialize.apply( this, arguments );
+            }
+        };
+
+        // Extend delegateEvents function to cater for selector keys
+        this.delegateEvents = function( events ) {
+            var match, eventName, selector, newSelector;
+
+            if ( !( events || ( events = _.result( this, 'events' ) ) ) ) {
+                return this;
+            }
+
+            // Loop over events
+            for ( var key in events ) {
+                // Split event and selector out from key
+                match = key.match( /^(\S+)\s*(.*)$/ );
+                eventName = match[ 1 ];
+                selector = match[ 2 ];
+                // If selector is a selector key
+                if ( this.hasSelector( selector ) ) {
+                    // Get the selector for the key and use that for delegate selector
+                    newSelector = this.selectors[ selector ];
+                    // Replace current key in events object with correct selector from selectors list
+                    events[ eventName + " " + newSelector ] = events[ key ];
+                    // Delete old event with selector name as no longer needed
+                    delete events[ key ];
+                }
+            }
+
+            // Call mixin (i.e. Backbone) delegateEvents function
+            return oldDelegateEvents.call( this, events );
+        };
+
+        // Get a selector from the view and cache it for later use
+        // If cached already, return cached selector
+        //
+        // Selectors should be on this.selectors which is a hash of
+        //
+        // * { "key" : "selector" } *
+        //
+        // {
+        //      "usernameField" : ".username",
+        //      "passwordField" : ".password"
+        // }
+        //
+        // pairs.
+        // Returns undefined if no key supplied or selector for given key.
+        this.get = function( key, refresh ) {
+            // Manual refresh of selector flag
+            refresh = refresh || false;
+            var element;
+
+            // If not manual refresh
+            // and is already cached selector
+            // and cached selector is still in the DOM within the view
+            if ( !refresh && this.isCachedSelector( key ) && this._selectors[ key ].closest( this.$el ).length > 0 ) {
+                // Use cached selector
+                element = this._selectors[ key ];
+            // Else if key has a selector defined
+            } else if ( this.hasSelector( key ) ) {
+                // Get selector and cache it
+                element = this._selectors[ key ] = this.$( this.selectors[ key ] );
+                if ( element.length === 0 ) {
+                    delete this._selectors[ key ];
+                    element = null;
+                }
+            }
+
+            // Return reference to selector
+            return element;
+        };
+
+        // Check if a selector key has a selector defined in the view
+        this.hasSelector = function( key ) {
+            return _.has( this.selectors, key );
+        };
+
+        // Check if a selector key has a valid selector in the view
+        this.isValidSelector = function( key ) {
+            return this.$( this.selectors[ key ] ).length > 0;
+        };
+
+        // Check if a selector key has already been cached
+        this.isCachedSelector = function( key ) {
+            return _.has( this._selectors, key );
+        };
+
+        // Clear a selector from cache or clear all cached references if no key is passed
+        this.clearSelector = function( key ) {
+            // If key has been supplied
+            if ( key ) {
+                // Is key cached
+                if ( this.isCachedSelector( key ) ) {
+                    // Remove cached key from list
+                    delete this._selectors[ key ];
+                }
+            } else {
+                // Reset cache object to empty object to remove all cached selectors
+                this._selectors = {};
+            }
+        };
+
+        // Clear selectors when view is removed to prevent any memory leaks
+        this.remove = function() {
+            this.clearSelector();
+            return oldRemove.apply( this, arguments );
+        };
+    };
+
+    return SelectorMixin;
+} ));
---
+++ test/specs/views/selectors/SelectorsSpec.js
@@ -0,0 +1,377 @@
+(function( root, factory ) {
+    // Set up appropriately for the environment.
+    // Start with AMD.
+    if ( typeof define === 'function' && define.amd ) {
+        define(
+            [
+                'backbone',
+                'jquery',
+                'views/selectors/Selectors'
+            ],
+            function( Backbone, $, SelectorMixin ) {
+                factory( Backbone, $, root.expect, SelectorMixin );
+            }
+        );
+    // Next for Node.js or CommonJS.
+    } else if ( typeof exports !== 'undefined' ) {
+        // Uses jquery which needs window object so don't run these tests in node
+    // Finally, as a browser global.
+    } else {
+        factory( root.Backbone, root.jQuery, root.expect, root.ExoSuit.Mixins.SelectorMixin );
+    }
+}( this, function( Backbone, $, expect, SelectorMixin ) {
+    describe('Selector Mixin View', function () {
+        var view, SelectorView, stub, stub2, oldMethodStub;
+
+        before(function() {
+            // If karma, add fixtures html
+            if ( typeof __html__ !== "undefined" ) {
+                document.body.innerHTML = __html__[ 'tests/specs/fixtures/main.html' ];
+            }
+            // Create fixture DOM placeholder
+            this.fixture = $( "<div id='selector-area'></div>" );
+            // Add fixture to DOM
+            this.fixture.appendTo( "#fixtures" );
+        });
+
+        after(function() {
+            // Empty any DOM created as part of this test suite
+            $( "#fixtures" ).empty();
+        });
+
+        describe('initialize()', function () {
+            beforeEach(function() {
+                // Create SelectorView definition
+                SelectorView = Backbone.View.extend({
+                    initialize: function( options ) {}
+                });
+                oldMethodStub = sinon.stub( SelectorView.prototype, "initialize" );
+                SelectorMixin.call( SelectorView.prototype );
+            });
+
+            afterEach(function() {
+                view.remove();
+            });
+
+            it('should set this._selectors empty object from the mixin initialize function', function() {
+                // Create view
+                view = new SelectorView();
+                expect( view._selectors ).to.be.an( 'object' );
+                expect( view._selectors ).to.be.empty;
+            });
+
+            it('should run the original initialize function too', function() {
+                // Create view
+                view = new SelectorView();
+                expect( oldMethodStub.called ).to.be.true;
+                expect( oldMethodStub.callCount ).to.equal( 1 );
+            });
+        });
+
+        describe('delegateEvents()', function () {
+            beforeEach(function() {
+                // Add dom to fixture
+                this.fixture.html( "<div id='selector-el'><button type='button' id='button'></div>" );
+                // Create View definition based on SelectorView module
+                SelectorView = Backbone.View.extend({
+                    el: "#selector-el",
+                    events: {
+                        "click myButton": "test"
+                    },
+                    selectors: {
+                        "myButton" : "#button"
+                    },
+                    test: function() {
+                        this.success = true;
+                    }
+                });
+                oldMethodStub = sinon.stub( SelectorView.prototype, "delegateEvents" );
+                SelectorMixin.call( SelectorView.prototype );
+            });
+
+            afterEach(function() {
+                // Clear down fixture DOM
+                this.fixture.empty();
+                oldMethodStub.restore();
+            });
+
+            it('should call the old delegateEvents function when the delegateEvents function is called with events object with correct selectors', function() {
+                // Create view
+                view = new SelectorView();
+                $( "#button" ).click();
+                expect( oldMethodStub.called ).to.be.true;
+                expect( oldMethodStub.callCount ).to.equal( 1 );
+                expect( oldMethodStub.calledWithMatch( { "click #button": "test" } ) ).to.be.true;
+                expect( oldMethodStub.neverCalledWithMatch( { "click myButton": "test" } ) ).to.be.true;
+            });
+        });
+
+        describe('get()', function () {
+            beforeEach(function() {
+                // Add dom to fixture
+                this.fixture.html( "<div id='selector-el'><input type='text' id='username'><input type='text' id='password'><div class='list'></div></div>" );
+                // Create SelectorView definition
+                SelectorView = Backbone.View.extend({
+                    el: "#selector-el",
+                    selectors: {
+                        "usernameField" : "#username",
+                        "passwordField" : "#password",
+                        "listDivs"      : ".list"
+                    }
+                });
+                SelectorMixin.call( SelectorView.prototype );
+            });
+
+            afterEach(function() {
+                // Clear down fixture DOM
+                this.fixture.empty();
+                // Clean up any stubs
+                stub.restore();
+                stub2.restore();
+                view.remove();
+            });
+
+            it('should return undefined if selector isnt defined in selector list', function() {
+                // Create view
+                view = new SelectorView();
+                // Stub function that is used internally to this method
+                stub = sinon.stub( view, "isCachedSelector" ).returns( false );
+                stub2 = sinon.stub( view, "hasSelector" ).returns( false );
+                expect( view.get( "noneExistingField" ) ).to.be.undefined;
+            });
+
+            it('should return a jquery object if selector has not been cached already and is in the selector list', function() {
+                // Create view
+                view = new SelectorView();
+                expect( view.isCachedSelector( "usernameField" ) ).to.be.false;
+                // Stub function that is used internally to this method
+                stub = sinon.stub( view, "isCachedSelector" ).returns( false );
+                stub2 = sinon.stub( view, "hasSelector" ).returns( true );
+                var element = view.get( "usernameField" );
+                expect( element ).to.exist;
+                expect( element.length ).to.equal( 1 );
+            });
+
+            it('should cache the selector in the _selectors object', function() {
+                // Create view
+                view = new SelectorView();
+                expect( view.isCachedSelector( "usernameField" ) ).to.be.false;
+                view.get( "usernameField" );
+                expect( view.isCachedSelector( "usernameField" ) ).to.be.true;
+            });
+
+            it('should return cached selector if selector has already been queried and cached', function() {
+                // Create view
+                view = new SelectorView();
+                expect( view.isCachedSelector( "usernameField" ) ).to.be.false;
+                view.get( "usernameField" );
+                expect( view.isCachedSelector( "usernameField" ) ).to.be.true;
+                stub = sinon.stub( view._selectors[ 'usernameField' ], "closest" ).returns( [ 1 ] );
+                $( "#username" ).remove();
+                var element = view.get( "usernameField" );
+                expect( element ).to.exist;
+                expect( element.length ).to.equal( 1 );
+            });
+
+            it('should re query the DOM if the cached selector is no longer in the view', function() {
+                // Create view
+                view = new SelectorView();
+                expect( view.isCachedSelector( "usernameField" ) ).to.be.false;
+                view.get( "usernameField" );
+                expect( view.isCachedSelector( "usernameField" ) ).to.be.true;
+                $( "#username" ).remove();
+                var element = view.get( "usernameField" );
+                expect( view.isCachedSelector( "usernameField" ) ).to.be.false;
+                expect( element ).not.to.exist;
+            });
+
+            it('should re query DOM if refresh flag is set to true', function() {
+                // Create view
+                view = new SelectorView();
+                expect( view.isCachedSelector( "listDivs" ) ).to.be.false;
+                var element = view.get( "listDivs" );
+                expect( element ).to.exist;
+                expect( element.length ).to.equal( 1 );
+                expect( view.isCachedSelector( "listDivs" ) ).to.be.true;
+                view.$el.append( "<div class='list'></div>" );
+                element = view.get( "listDivs", true );
+                expect( element ).to.exist;
+                expect( element.length ).to.equal( 2 );
+            });
+        });
+
+        describe('hasSelector()', function () {
+            beforeEach(function() {
+                // Create SelectorView definition
+                SelectorView = Backbone.View.extend({
+                    selectors: {
+                        "usernameField": "#username",
+                        "passwordField": "#password"
+                    }
+                });
+                SelectorMixin.call( SelectorView.prototype );
+            });
+
+            afterEach(function() {
+                view.remove();
+            });
+
+            it('should return true for keys defined within selectors list', function() {
+                // Create view
+                view = new SelectorView();
+                expect( view.hasSelector( "usernameField" ) ).to.be.true;
+                expect( view.hasSelector( "passwordField" ) ).to.be.true;
+            });
+
+            it('should return false for keys not defined within selectors list', function() {
+                // Create view
+                view = new SelectorView();
+                expect( view.hasSelector( "submitButton" ) ).to.be.false;
+            });
+        });
+
+        describe('isValidSelector()', function () {
+            beforeEach(function() {
+                // Add dom to fixture
+                this.fixture.html( "<div id='selector-el'><input type='text' id='username'><input type='text' id='password'></div>" );
+                // Create SelectorView definition
+                SelectorView = Backbone.View.extend({
+                    el: "#selector-el",
+                    selectors: {
+                        "usernameField" : "#username",
+                        "passwordField" : "#password",
+                        "confirmField"  : "#confirm"
+                    }
+                });
+                SelectorMixin.call( SelectorView.prototype );
+                // Create view
+                view = new SelectorView();
+            });
+
+            afterEach(function() {
+                // Clear down fixture DOM
+                this.fixture.empty();
+                view.remove();
+            });
+
+            it('should return true for a key with a valid selector within the Views DOM area', function() {
+                expect( view.isValidSelector( "usernameField" ) ).to.be.true;
+                expect( view.isValidSelector( "passwordField" ) ).to.be.true;
+            });
+
+            it('should return false for a key with a selector that isnt within the Views DOM area', function() {
+                expect( view.isValidSelector( "confirmField" ) ).to.be.false;
+            });
+        });
+
+        describe('isCachedSelector()', function () {
+            beforeEach(function() {
+                // Create SelectorView definition
+                SelectorView = Backbone.View.extend();
+                SelectorMixin.call( SelectorView.prototype );
+                // Create view
+                view = new SelectorView();
+                // Fake a set of predefined cached selectors - just a hash needed, values don't matter
+                view._selectors = {
+                    "usernameField": "value",
+                    "passwordField": "anotherValue"
+                };
+            });
+
+            afterEach(function() {
+                view.remove();
+            });
+
+            it('should return true for keys that have cached selectors', function() {
+                expect( view.isCachedSelector( "usernameField" ) ).to.be.true;
+                expect( view.isCachedSelector( "passwordField" ) ).to.be.true;
+            });
+
+            it('should return false for keys that do not have cached selectors', function() {
+                expect( view.isCachedSelector( "submitButton" ) ).to.be.false;
+            });
+        });
+
+        describe('clearSelector()', function () {
+            beforeEach(function() {
+                // Create SelectorView definition
+                SelectorView = Backbone.View.extend();
+                SelectorMixin.call( SelectorView.prototype );
+            });
+
+            afterEach(function() {
+                // Clean up any stubs
+                stub.restore();
+                view.remove();
+            });
+
+            it('should remove cached selector if key is supplied and is already cached', function() {
+                // Stub function that is used internally to this method
+                stub = sinon.stub( SelectorView.prototype, "isCachedSelector" ).returns( true );
+                // Create view
+                view = new SelectorView();
+                // Fake a set of predefined cached selectors - just a hash needed, values don't matter
+                view._selectors = {
+                    "testKey": "value"
+                };
+                expect( view._selectors ).to.be.an( 'object' );
+                expect( view._selectors ).to.not.be.empty;
+                view.clearSelector( "testKey" );
+                expect( view._selectors ).to.be.an( 'object' );
+                expect( view._selectors ).to.be.empty;
+            });
+
+            it('should remove all cached selectors if no key is passed', function() {
+                // Create view
+                view = new SelectorView();
+                // Fake a set of predefined cached selectors - just a hash needed, values don't matter
+                view._selectors = {
+                    "testKey": "value",
+                    "anotherTest": "anotherValue"
+                };
+                expect( view._selectors ).to.be.an( 'object' );
+                expect( view._selectors ).to.not.be.empty;
+                view.clearSelector();
+                expect( view._selectors ).to.be.an( 'object' );
+                expect( view._selectors ).to.be.empty;
+            });
+        });
+
+        describe('remove()', function () {
+            beforeEach(function() {
+                // Create SelectorView definition
+                SelectorView = Backbone.View.extend();
+                oldMethodStub = sinon.stub( SelectorView.prototype, "remove" );
+                SelectorMixin.call( SelectorView.prototype );
+            });
+
+            afterEach(function() {
+                // Clean up any stubs
+                stub.restore();
+                oldMethodStub.restore();
+            });
+
+            it('should call clearSelector when remove is called', function() {
+                // Stub function that is used internally to this method
+                stub = sinon.stub( SelectorView.prototype, "clearSelector" );
+                // Create view
+                view = new SelectorView();
+                view.remove();
+                expect( stub.called ).to.be.true;
+                expect( stub.callCount ).to.equal( 1 );
+                expect( stub.calledWith() ).to.be.true;
+            });
+
+            it('should call old remove method when remove is called', function() {
+                // Stub function that is used internally to this method
+                stub = sinon.stub( SelectorView.prototype, "clearSelector" );
+                // Create view
+                view = new SelectorView();
+                view.remove();
+                expect( oldMethodStub.called ).to.be.true;
+                expect( oldMethodStub.callCount ).to.equal( 1 );
+                expect( oldMethodStub.calledWith() ).to.be.true;
+            });
+        });
+    });
+}));
