660541998936d84a2af59e5deb644bafaf41c9cb
Uplift <git@upliftingcode.com>
2014-11-18 13:55:40 +0000

    Update index.js with all committed mixins

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var ExoSuit = {
     "Mixins": {
-        "ExcludeFromJSONMixin": require( './src/models/exclude-from-json/ExcludeFromJson' )
+        "ClientPaginationMixin": require( './src/collections/pagination/ClientPagination' ),
+        "ExcludeFromJSONMixin": require( './src/models/exclude-from-json/ExcludeFromJson' ),
+        "GoogleAnalyticsRouteMixin": require( './src/routers/analytics/Google' ),
+        "ParseQueryStringMixin": require( './src/routers/parse/ParseQueryString' ),
+        "RouteTitleMixin": require( './src/routers/route-title/RouteTitle' ),
+        "DataBindingMixin": require( './src/views/data-binding/DataBinding' ),
+        "SelectorMixin": require( './src/views/selectors/Selectors' )
     }
 };
 
