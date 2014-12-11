97c8e6fb130f91805697a42aa5b0fa6ecee685e5
Uplift <git@upliftingcode.com>
2014-12-11 12:09:56 +0000

    Add SortOnChange mixin to node index file and fix old ModelBinding name

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var ExoSuit = {
     "Mixins": {
         "CappedCollectionMixin": require( './src/collections/capped/CappedCollection' ),
         "ClientPaginationMixin": require( './src/collections/pagination/ClientPagination' ),
+        "SortOnChangeMixin": require( './src/collections/sort-on-change/SortOnChange' ),
         "ExcludeFromJSONMixin": require( './src/models/exclude-from-json/ExcludeFromJson' ),
         "GoogleAnalyticsRouteMixin": require( './src/routers/analytics/Google' ),
         "ParseQueryStringMixin": require( './src/routers/parse/ParseQueryString' ),
         "RouteTitleMixin": require( './src/routers/route-title/RouteTitle' ),
-        "DataBindingMixin": require( './src/views/data-binding/DataBinding' ),
+        "ModelBindingMixin": require( './src/views/data-binding/ModelBinding' ),
         "SelectorMixin": require( './src/views/selectors/Selectors' )
     }
 };
