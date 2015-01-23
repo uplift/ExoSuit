708187af297ee748eda34498e16cb26e81edffcf
Uplift <git@upliftingcode.com>
2015-01-23 13:03:46 +0000

    Add Slect List view mixin for select dropdowns based on collection data

---
+++ src/views/select-list/Select.js
@@ -0,0 +1,81 @@
+( function( root, factory ) {
+    "use strict";
+    // Set up appropriately for the environment.
+    // Start with AMD.
+    if ( typeof define === 'function' && define.amd ) {
+        define(
+            function() {
+                root.ExoSuit = root.ExoSuit || {};
+                root.ExoSuit.Mixins = root.ExoSuit.Mixins || {};
+                return ( root.ExoSuit.Mixins.SelectListMixin = factory( root, {} ) );
+            }
+        );
+    // Next for Node.js or CommonJS.
+    } else if ( typeof exports !== 'undefined' ) {
+        module.exports = factory( root, exports );
+    // Finally, as a browser global.
+    } else {
+        root.ExoSuit = root.ExoSuit || {};
+        root.ExoSuit.Mixins = root.ExoSuit.Mixins || {};
+        root.ExoSuit.Mixins.SelectListMixin = factory( root, {} );
+    }
+}( this, function( root, SelectListMixin ) {
+    "use strict";
+    SelectListMixin = function() {
+        var oldInitialize = this.initialize;
+
+        this.tagName = "select";
+        this.labelKey = this.labelKey || "label";
+        this.valueKey = this.valueKey || "value"
+        this.addInitialOption = this.addInitialOption || false;
+        this.initialOptionLabel = typeof this.initialOptionLabel !== "undefined" ? this.initialOptionLabel : "Please Select";
+        this.initialOptionValue = typeof this.initialOptionValue !== "undefined" ? this.initialOptionValue : "0";
+
+        this.initialize = function( options ) {
+            options = options || {};
+
+            if ( options.labelKey ) {
+                this.labelKey = options.labelKey;
+            }
+            if ( options.valueKey ) {
+                this.valueKey = options.valueKey;
+            }
+            if ( options.addInitialOption ) {
+                this.addInitialOption = options.addInitialOption;
+            }
+            if ( typeof options.initialOptionLabel !== "undefined" ) {
+                this.initialOptionLabel = options.initialOptionLabel;
+            }
+            if ( typeof options.initialOptionValue !== "undefined" ) {
+                this.initialOptionValue = options.initialOptionValue;
+            }
+            if ( typeof options.selectedIndex !== "undefined" ) {
+                this.selectedIndex = options.selectedIndex;
+            }
+
+            if ( oldInitialize ) {
+                oldInitialize.apply( this, arguments );
+            }
+        }
+
+        this.render = function() {
+            var options = "";
+
+            if ( this.addInitialOption ) {
+                options += "<option value='" + this.initialOptionValue + "'>" + this.initialOptionLabel + "</option>"
+            }
+
+            if ( this.collection ) {
+                this.collection.forEach( function( model, index ) {
+                    var selected = this.selectedIndex && this.selectedIndex === index ? " selected" : "";
+
+                    options += "<option value='" + model.get( this.valueKey ) + "'" + selected + ">" + model.get( this.labelKey ) + "</option>";
+                }, this);
+            }
+
+            return this.$el.html( options );
+        };
+    };
+
+    return SelectListMixin;
+} ));
---
+++ test/specs/views/select-list/SelectSpec.js
@@ -0,0 +1,163 @@
+(function( root, factory ) {
+    // Set up appropriately for the environment.
+    // Start with AMD.
+    if ( typeof define === 'function' && define.amd ) {
+        define(
+            [
+                'backbone',
+                'views/select-list/Select'
+            ],
+            function( Backbone, SelectListMixin ) {
+                factory( Backbone, root.expect, SelectListMixin );
+            }
+        );
+    // Next for Node.js or CommonJS.
+    } else if ( typeof exports !== 'undefined' ) {
+        // Uses jquery which needs window object so don't run these tests in node
+    // Finally, as a browser global.
+    } else {
+        factory( root.Backbone, root.expect, root.ExoSuit.Mixins.SelectListMixin );
+    }
+}( this, function( Backbone, expect, SelectListMixin ) {
+    describe('Select List Mixin View', function () {
+        var view, SelectView, oldMethodStub;
+
+        describe('initialize()', function () {
+            beforeEach(function() {
+                // Create SelectorView definition
+                SelectView = Backbone.View.extend({
+                    initialize: function( options ) {}
+                });
+                oldMethodStub = sinon.stub( SelectView.prototype, "initialize" );
+                SelectListMixin.call( SelectView.prototype );
+            });
+
+            afterEach(function() {
+                view.remove();
+            });
+
+            it('should override default select list options if passed in options hash to initialize', function() {
+                // Create view
+                view = new SelectView({
+                    labelKey: "myLabel",
+                    valueKey: "myValue",
+                    addInitialOption: true,
+                    initialOptionLabel: "Select Value",
+                    initialOptionValue: "100",
+                    selectedIndex: 1
+                });
+                expect( view.labelKey ).to.equal( 'myLabel' );
+                expect( view.valueKey ).to.equal( 'myValue' );
+                expect( view.addInitialOption ).to.be.true;
+                expect( view.initialOptionLabel ).to.equal( 'Select Value' );
+                expect( view.initialOptionValue ).to.equal( '100' );
+                expect( view.selectedIndex ).to.equal( 1 );
+            });
+
+            it('should call old initialize method', function() {
+                // Create view
+                view = new SelectView();
+                expect( oldMethodStub.called ).to.be.true;
+                expect( oldMethodStub.callCount ).to.equal( 1 );
+            });
+        });
+
+        describe('render()', function () {
+            beforeEach(function() {
+                // Create SelectorView definition
+                SelectView = Backbone.View.extend();
+                SelectListMixin.call( SelectView.prototype );
+            });
+
+            afterEach(function() {
+                view.remove();
+            });
+
+            it('should add the models of the collection as option tags within the select element of the view', function() {
+                // Create view
+                view = new SelectView({
+                    collection: new Backbone.Collection( [
+                        {
+                            label: "One",
+                            value: "1"
+                        },
+                        {
+                            label: "Two",
+                            value: "2"
+                        },
+                        {
+                            label: "Three",
+                            value: "3"
+                        }
+                    ] )
+                });
+                expect( view.render().html() ).to.equal( '<option value="1">One</option><option value="2">Two</option><option value="3">Three</option>' );
+            });
+
+            it('should add an initial option if set to true', function() {
+                // Create view
+                view = new SelectView({
+                    addInitialOption: true,
+                    collection: new Backbone.Collection( [
+                        {
+                            label: "One",
+                            value: "1"
+                        },
+                        {
+                            label: "Two",
+                            value: "2"
+                        },
+                        {
+                            label: "Three",
+                            value: "3"
+                        }
+                    ] )
+                });
+                expect( view.render().html() ).to.equal( '<option value="0">Please Select</option><option value="1">One</option><option value="2">Two</option><option value="3">Three</option>' );
+                // Create view
+                view = new SelectView({
+                    addInitialOption: true,
+                    initialOptionLabel: "Select Value",
+                    initialOptionValue: "",
+                    collection: new Backbone.Collection( [
+                        {
+                            label: "One",
+                            value: "1"
+                        },
+                        {
+                            label: "Two",
+                            value: "2"
+                        },
+                        {
+                            label: "Three",
+                            value: "3"
+                        }
+                    ] )
+                });
+                expect( view.render().html() ).to.equal( '<option value="">Select Value</option><option value="1">One</option><option value="2">Two</option><option value="3">Three</option>' );
+            });
+
+            it('should ', function() {
+                // Create view
+                view = new SelectView({
+                    selectedIndex: 2,
+                    collection: new Backbone.Collection( [
+                        {
+                            label: "One",
+                            value: "1"
+                        },
+                        {
+                            label: "Two",
+                            value: "2"
+                        },
+                        {
+                            label: "Three",
+                            value: "3"
+                        }
+                    ] )
+                });
+                expect( view.render().html() ).to.equal( '<option value="1">One</option><option value="2">Two</option><option value="3" selected="">Three</option>' );
+            });
+        });
+    });
+}));
